10209567b47ef8c130075959975ee5dc81399167
pribhat06 <pri.bhat.95@gmail.com>
2023-06-06 15:27:13 +0530

    gus

--- oops/abstraction.py
+++ oops/abstraction.py
@@ -2,13 +2,13 @@ from abc import ABC, abstractclassmethod
 
 class Car(ABC):
     @abstractclassmethod
-    def mileage(self):
+    def mileage(self): #different implementation in class and sub classes--> abstract method
         pass
-    def color(self):
+    def color(self): # same implementation in all sub classes-->concreate method
         print("while")
     
-class Maruti_Suzuki(Car):
-    def mileage(self):
+class Maruti_Suzuki(Car): # if we can't define abstract method then it will through an error
+    def mileage(self):    # so if we don't define abstract method in child class, so child class will become abstract Class
         print("Mileage is 30 kmph")
     
 class TATA(Car):
@@ -29,3 +29,33 @@ d1=Duster()
 
 t1.mileage()
 t1.color()
+
+
+print("**ABSTRACT**")
+from abc import ABC, abstractmethod
+class BankApp(ABC):
+    def database(self):
+        print("connected to abstract")
+
+    @abstractmethod
+    def security(self):
+        pass
+    @abstractmethod
+    def display(self):
+        pass
+
+class MobileApp(BankApp):
+    
+    def mobile_login(self):
+        print("login into mobile")
+
+    def security(self):
+        print("Mobile security")
+
+    def display(self):
+        print("Mobile disply")
+
+mob = MobileApp()
+mob.security()
+
+
